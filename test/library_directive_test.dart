8a31fc12a44ec0cac4c94e22db774d7abafcb81b
Bruno D'Luka <brunodlukaa@gmail.com>
2023-12-24 16:46:10 -0300

    chore: remove augment test

--- test/library_directive_test.dart
+++ test/library_directive_test.dart
@@ -9,17 +9,6 @@ void main() {
         parseAsJson('library foo;'),
         {'kind': 'library', 'name': 'foo'},
       );
-    });
-
-    // test('augment library', () {
-    //   expect(
-    //     parseAsJson("library augment 'main_library.dart';"),
-    //     {
-    //       'kind': 'library',
-    //       'augmentation': true,
-    //       'uri': 'main_library.dart',
-    //     },
-    //   );
-    // });
+    });    
   });
 }
